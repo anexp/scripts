ce41bda0198960c2b4203d51eeab04bd9d082a0e
Anirban Dey <andy@soufrabi.com>
2025-05-11 21:18:19 +0530

    Control whether to notify upon clearing clipboard

--- bin/clipboard_clear.sh
+++ bin/clipboard_clear.sh
@@ -1,27 +1,47 @@
 #!/bin/sh
 
+notification_flag=true
 
 clear_clipboard() {
 
-case "$XDG_SESSION_TYPE" in
-	("x11")
-		printf "In x11 session \n"
+    case "$XDG_SESSION_TYPE" in
+    "x11")
+        printf "In x11 session \n"
         # printf "" | xclip -sel clip
         xsel -bc &
 
         # current_clipboard_content="$(xclip -sel clip)"
-        notify-send -u low "Cleared Clipboard X11 CurValue=[${current_clipboard_content}"] &
-		;;
-	("wayland")
-		printf "In wayland session \n"
+        if [ "$notification_flag" = "true" ]; then
+            # notify-send -u low "Cleared Clipboard X11 CurValue=[${current_clipboard_content}]" &
+            notify-send -u low "Cleared Clipboard X11" &
+        fi
+        ;;
+    "wayland")
+        printf "In wayland session \n"
         wl-copy --clear &
-		;;
-	(*)
-		printf "Neither x11 or wayland \n"
-		;;
-esac
+        if [ "$notification_flag" = "true" ]; then
+            notify-send -u low "Cleared Clipboard Wayland" &
+        fi
+        ;;
+    *)
+        printf "Neither x11 or wayland session. Failed to clear clipboard.\n"
+        ;;
+    esac
 
 }
 
+main() {
+    case "$1" in
+    no-notify)
+        shift
+        notification_flag=false
+        ;;
+    *)
+        notification_flag=true
+        ;;
+    esac
 
-clear_clipboard &
+    clear_clipboard &
+}
+
+main "$@"
